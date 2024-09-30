7128e6e269fb41e566604b3ab823cbdf129b0679
313 <313@DESKTOP-R53GIRH>
2024-09-30 16:07:37 +0900

    first commit

---
+++ basicexample/src/main/java/com/platformtechlab/msf/basicExample/EmployeeController.java
@@ -0,0 +1,86 @@
+package com.platformtechlab.msf.basicExample;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+ 
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to RestTemplate Example.";
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employees
+    @RequestMapping(value = "/employees", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public List<Employee> getEmployees() {
+        List<Employee> list = employeeDAO.getAllEmployees();
+        return list;
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee/{empNo}
+    @RequestMapping(value = "/employee/{empNo}", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public Employee getEmployee(@PathVariable("empNo") String empNo) {
+        return employeeDAO.getEmployee(empNo);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee
+
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.POST, //
+            produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public Employee addEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Creating employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.addEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.PUT, //
+            produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public Employee updateEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Editing employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.updateEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee/{empNo}
+    @RequestMapping(value = "/employee/{empNo}", //
+            method = RequestMethod.DELETE, //
+            produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public void deleteEmployee(@PathVariable("empNo") String empNo) {
+ 
+        System.out.println("(Service Side) Deleting employee: " + empNo);
+ 
+        employeeDAO.deleteEmployee(empNo);
+    }
+}
---
+++ basicexample/src/main/java/com/platformtechlab/msf/basicExample/basicExampleApp.java
@@ -0,0 +1,12 @@
+package com.platformtechlab.msf.basicExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class basicExampleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(basicExampleApp.class, args);
+    }
+}
